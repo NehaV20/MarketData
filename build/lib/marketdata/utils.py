03551b027477e9e511b1b2e2ccf06882428a6b13
NehaV20 <n.verma@quantxpress.com>
2025-11-28 08:02:43 +0530

    Remove unnecessary files

--- build/lib/marketdata/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def json_response(response):
-    """Returns JSON response or logs error"""
-    try:
-        return response.json()
-    except json.JSONDecodeError:
-        return {"error": "Failed to decode JSON"}
